826fb69fb4a007876db8a75155287a84129840c5
karan <karandeepsangha95@gmail.com>
2023-05-03 18:56:42 -0700

    view change

--- src/SQLite/Database.java
+++ src/SQLite/Database.java
@@ -1,4 +1,5 @@
 package SQLite;
 
 public class Database {
+    
 }
--- src/View/Main.java
+++ src/View/Main.java
@@ -1,10 +1,5 @@
 package View;
 public class Main {
-<<<<<<<<< Temporary merge branch 1
-    public static void main(String[] args) {
-        System.out.println("It is the view");
-        // Hello Raine
-=========
     // :3
     public static void main(String[] args) {
         System.out.println("amanda<3QRWIII");
