82f080762c3642309864037e2b282b043e136cc4
Amanda Nguyen <amandanguyen65@gmail.com>
2023-05-11 23:05:53 -0700

    Added getters to Options, updated Reality Stone

--- src/Model/BackpackTest.java
+++ src/Model/BackpackTest.java
@@ -1,4 +1,19 @@
 package Model;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
 public class BackpackTest {
+    /** Create instance of Player class to use in the tests */
+    private Backpack myBackpack;
+
+    @BeforeEach
+    void setUp() {
+        myBackpack = new Backpack();
+    }
+    @Test
+    void testAddToBackpack() {
+        myBackpack.addToBackpack(new MindStone());
+
+    }
 }
