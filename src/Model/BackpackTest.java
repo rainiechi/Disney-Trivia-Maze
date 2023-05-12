5fe9099a1c9f92ccac8984d7686cb5fe740305a9
Amanda Nguyen <amandanguyen65@gmail.com>
2023-05-11 23:34:18 -0700

    Added getters to Options, updated Reality Stone

--- src/Model/BackpackTest.java
+++ src/Model/BackpackTest.java
@@ -14,6 +14,43 @@ public class BackpackTest {
     @Test
     void testAddToBackpack() {
         myBackpack.addToBackpack(new MindStone());
+        assertEquals(1, myBackpack.getCurrItems());
+    }
+    @Test
+    void testAddToBackpackFull() {
+        myBackpack.addToBackpack(new MindStone());
+        myBackpack.addToBackpack(new SoulStone());
+        myBackpack.addToBackpack(new PowerStone());
+        myBackpack.addToBackpack(new TimeStone());
+        myBackpack.addToBackpack(new SpaceStone());
+        myBackpack.addToBackpack(new RealityStone());
+        assertEquals(6, myBackpack.getCurrItems());
+    }
+    void testAddToBackpackPastMax() {
+        myBackpack.addToBackpack(new MindStone());
+        myBackpack.addToBackpack(new SoulStone());
+        myBackpack.addToBackpack(new PowerStone());
+        myBackpack.addToBackpack(new TimeStone());
+        myBackpack.addToBackpack(new SpaceStone());
+        myBackpack.addToBackpack(new RealityStone());
 
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> myBackpack.addToBackpack(new RealityStone()),
+                "Expected addToBackpack() to throw, but it didn't"
+        );
+
+        assertTrue(thrown.getMessage().equals("Game error, there should be no more than 6 stones in game"));
+    }
+    @Test
+    void testUseStonePlayerDoesNotHave() {
+        myBackpack.addToBackpack(new MindStone());
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> myBackpack.useStone(new RealityStone()),
+                "Expected useStone() to throw, but it didn't"
+        );
+
+        assertTrue(thrown.getMessage().equals("Player does not have this stone."));
     }
 }
