dc9fbc791e78ff52c9417a4b97c6e36997279306
Amanda Nguyen <amandanguyen65@gmail.com>
2023-05-15 01:02:29 -0700

    Added 2D array of maze, created loadMap method to read file, created draw method to draw tiles based off 2D array, added graphical assets into folders, modified game settings to fit map.

---
+++ src/View/KeyHandler.java
@@ -0,0 +1,79 @@
+package View;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+    private boolean upPressed, downPressed, rightPressed, leftPressed;
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // not needed
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int code = e.getKeyCode();
+        if (code == KeyEvent.VK_W) {
+            setUpPressed(true);
+        }
+        if (code == KeyEvent.VK_S) {
+            setDownPressed(true);
+        }
+        if (code == KeyEvent.VK_A) {
+            setLeftPressed(true);
+        }
+        if (code == KeyEvent.VK_D) {
+            setRightPressed(true);
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int code = e.getKeyCode();
+        if (code == KeyEvent.VK_W) {
+            setUpPressed(false);
+        }
+        if (code == KeyEvent.VK_S) {
+            setDownPressed(false);
+        }
+        if (code == KeyEvent.VK_A) {
+            setLeftPressed(false);
+        }
+        if (code == KeyEvent.VK_D) {
+            setRightPressed(false);
+        }
+    }
+
+    public boolean isUpPressed() {
+        return upPressed;
+    }
+
+    public boolean setUpPressed(boolean upPressed) {
+        this.upPressed = upPressed;
+        return upPressed;
+    }
+
+    public boolean isDownPressed() {
+        return downPressed;
+    }
+
+    public void setDownPressed(boolean downPressed) {
+        this.downPressed = downPressed;
+    }
+
+    public boolean isRightPressed() {
+        return rightPressed;
+    }
+
+    public void setRightPressed(boolean rightPressed) {
+        this.rightPressed = rightPressed;
+    }
+
+    public boolean isLeftPressed() {
+        return leftPressed;
+    }
+
+    public void setLeftPressed(boolean leftPressed) {
+        this.leftPressed = leftPressed;
+    }
+}
