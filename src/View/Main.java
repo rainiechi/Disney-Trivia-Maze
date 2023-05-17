d513088c8155df0479d02b9666bbc4a301884991
Rainie Chi <lemoncc1222@gmail.com>
2023-05-17 13:33:30 -0700

    Added GameFrame class to incorporate menuBar

--- src/View/Main.java
+++ src/View/Main.java
@@ -1,20 +1,25 @@
 package View;
 import javax.swing.JFrame;
     public class Main {
+//        public static void main(String[] args) {
+//            JFrame window = new JFrame();
+//            window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//            window.setResizable(false);
+//            window.setTitle("Disney Trivia Maze");
+//
+//            GamePanel gamePanel = new GamePanel();
+//            window.add(gamePanel);
+//
+//            window.pack();
+//
+//            window.setLocationRelativeTo(null);
+//            window.setVisible(true);
+//
+//            gamePanel.startGameThread();
+//        }
         public static void main(String[] args) {
-            JFrame window = new JFrame();
-            window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            window.setResizable(false);
-            window.setTitle("Disney Trivia Maze");
-
             GamePanel gamePanel = new GamePanel();
-            window.add(gamePanel);
-
-            window.pack();
-
-            window.setLocationRelativeTo(null);
-            window.setVisible(true);
-
+            new GameFrame(gamePanel);
             gamePanel.startGameThread();
         }
     }
