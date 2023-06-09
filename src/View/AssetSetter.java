b047cb1b052ada50ab34cc48af4d02f4cb11a8dd
Rainie Chi <lemoncc1222@gmail.com>
2023-06-09 00:46:32 -0700

    visibility fix

--- src/View/AssetSetter.java
+++ src/View/AssetSetter.java
@@ -14,14 +14,14 @@ public class AssetSetter {
     private static final int TILE_SIZE = GameSettings.TILE_SIZE;
 
     /** private array of ObjectManager field */
-    private final ObjectManager[] myObjManager;
+    private final ObjectManager[] myObjManagers;
 
     /**
      * Constructor initializes fields.
      * @param theObjManager Array of ObjectManager
      */
     public AssetSetter(final ObjectManager[] theObjManager) {
-        this.myObjManager = theObjManager;
+        this.myObjManagers = theObjManager;
         try {
             setWhiteDoors();
             setChests();
@@ -35,13 +35,13 @@ public class AssetSetter {
     /**
      * Method sets the coordinates of front facing white doors on the map.
      */
-    public void setWhiteDoors() {
+    private void setWhiteDoors() {
         // Starts at (8,14) on the map
         int x = 8;
         int y = 14;
         for (int i = 0; i <= 19; i++) {
             // sets doors by going down rows.
-            myObjManager[i] = new ObjectManager("Door", x * TILE_SIZE, y * TILE_SIZE, true);
+            myObjManagers[i] = new ObjectManager("Door", x * TILE_SIZE, y * TILE_SIZE, true);
             y += 13;
             // Starts at a new column on the map.
             if (i == 3 || i == 7 || i == 10 || i == 14 || i == 18) {
@@ -56,15 +56,15 @@ public class AssetSetter {
     /**
      * Sets exit door location on the map.
      */
-    public void ExitLocation() {
+    private void ExitLocation() {
         // location of exit door is (38,1) on world map.
-        myObjManager[20] = new ObjectManager("Exit", 38 * TILE_SIZE, 1 * TILE_SIZE, true);
+        myObjManagers[20] = new ObjectManager("Exit", 38 * TILE_SIZE, 1 * TILE_SIZE, true);
     }
 
     /**
      * Sets side door locations on the map.
      */
-    public void setSideDoors() {
+    private void setSideDoors() {
         // Starts at (12,2) on the map
         int x = 12;
         int y = 3;
@@ -73,7 +73,7 @@ public class AssetSetter {
             // sets side doors by moving column to column.
             // if statements are used to move to the next row
             // or skip a column.
-            myObjManager[i] = new ObjectManager("SideDoor", x * TILE_SIZE, y * TILE_SIZE, true);
+            myObjManagers[i] = new ObjectManager("SideDoor", x * TILE_SIZE, y * TILE_SIZE, true);
             if (count == 2) {
                 x += 8;
                 count = 0;
@@ -97,15 +97,15 @@ public class AssetSetter {
     /**
      * Method sets the coordinates of chests on the map.
      */
-    public void setChests() {
-        myObjManager[53] = new ObjectManager("Chest", 8 * TILE_SIZE, 2 * TILE_SIZE, true);
-        myObjManager[54] = new ObjectManager("Chest", 53 * TILE_SIZE, 16 * TILE_SIZE, true);
-        myObjManager[55] = new ObjectManager("Chest", 23 * TILE_SIZE, 16 * TILE_SIZE, true);
-        myObjManager[56] = new ObjectManager("Chest", 8 * TILE_SIZE, 29 * TILE_SIZE, true);
-        myObjManager[57] = new ObjectManager("Chest", 68 * TILE_SIZE, 2 * TILE_SIZE, true);
-        myObjManager[58] = new ObjectManager("Chest", 23 * TILE_SIZE, 55 * TILE_SIZE, true);
-        myObjManager[59] = new ObjectManager("Chest", 68 * TILE_SIZE, 29 * TILE_SIZE, true);
-        myObjManager[60] = new ObjectManager("Chest", 53 * TILE_SIZE, 42 * TILE_SIZE, true);
+    private void setChests() {
+        myObjManagers[53] = new ObjectManager("Chest", 8 * TILE_SIZE, 2 * TILE_SIZE, true);
+        myObjManagers[54] = new ObjectManager("Chest", 53 * TILE_SIZE, 16 * TILE_SIZE, true);
+        myObjManagers[55] = new ObjectManager("Chest", 23 * TILE_SIZE, 16 * TILE_SIZE, true);
+        myObjManagers[56] = new ObjectManager("Chest", 8 * TILE_SIZE, 29 * TILE_SIZE, true);
+        myObjManagers[57] = new ObjectManager("Chest", 68 * TILE_SIZE, 2 * TILE_SIZE, true);
+        myObjManagers[58] = new ObjectManager("Chest", 23 * TILE_SIZE, 55 * TILE_SIZE, true);
+        myObjManagers[59] = new ObjectManager("Chest", 68 * TILE_SIZE, 29 * TILE_SIZE, true);
+        myObjManagers[60] = new ObjectManager("Chest", 53 * TILE_SIZE, 42 * TILE_SIZE, true);
     }
 
 }
