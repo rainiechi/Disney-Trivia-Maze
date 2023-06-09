8069edb98676c8ec5408c62da7fac7a7f084b425
karan <karandeepsangha95@gmail.com>
2023-06-09 09:47:25 -0700

    Completed Installer

--- src/SQLite/DBRetriever.java
+++ src/SQLite/DBRetriever.java
@@ -3,10 +3,7 @@ package SQLite;
 import Model.Question;
 import org.sqlite.SQLiteDataSource;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 /**
  * DBRetriever accesses the SQL database and retrieves questions.
@@ -14,9 +11,8 @@ import java.sql.Statement;
 public class DBRetriever {
     private SQLiteDataSource myDs;
 
-
     /**
-     * DBRetriver constructor
+     * DBRetriever constructor
      */
     public DBRetriever() {
         myDs = buildConnection();
@@ -24,23 +20,25 @@ public class DBRetriever {
 
     /**
      * Builds connection with database RealQuestions
+     *
      * @return SQLiteDataSource
      */
     public SQLiteDataSource buildConnection() {
         SQLiteDataSource ds = null;
         try {
             ds = new SQLiteDataSource();
-            ds.setUrl("jdbc:sqlite:src/SQLite/RealQuestions.db");
-        } catch ( Exception e ) {
+            ds.setUrl("jdbc:sqlite::resource:SQLite/RealQuestions.db");
+        } catch (Exception e) {
             e.printStackTrace();
             System.exit(0);
         }
-        System.out.println( "Opened database successfully" );
+        System.out.println("Opened database successfully");
         return ds;
     }
 
     /**
      * Gets how many questions are in the database.
+     *
      * @return how many questions are in the database
      */
     public int getEntryCount() {
@@ -60,6 +58,7 @@ public class DBRetriever {
 
         return entryCount;
     }
+
     /**
      * Retrieves a random question that has not been used, then marks the question as used once retrieved.
      */
@@ -67,29 +66,25 @@ public class DBRetriever {
         Question question = null;
         String query = "SELECT * FROM questions ORDER BY RANDOM() LIMIT 1";
 
-        try ( Connection conn = myDs.getConnection();
-              Statement stmt = conn.createStatement(); ) {
-            ResultSet rs = stmt.executeQuery(query);
-            query = "SELECT * FROM questions ORDER BY RANDOM() LIMIT 1";
-            rs = stmt.executeQuery(query);
-
-
-            question = new Question(rs.getString( "QUESTION" ),
-                    rs.getString( "ANSWER" ),
-                    rs.getString( "CHOICE1" ),
-                    rs.getString( "CHOICE2" ),
-                    rs.getString( "CHOICE3" ),
-                    rs.getString( "CHOICE4" ),
-                    rs.getInt("ID"));
-
-
-        } catch ( SQLException e ) {
+        try (Connection conn = myDs.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+            if (rs.next()) {
+                question = new Question(
+                        rs.getString("QUESTION"),
+                        rs.getString("ANSWER"),
+                        rs.getString("CHOICE1"),
+                        rs.getString("CHOICE2"),
+                        rs.getString("CHOICE3"),
+                        rs.getString("CHOICE4"),
+                        rs.getInt("ID")
+                );
+            }
+        } catch (SQLException e) {
             e.printStackTrace();
-            System.exit( 0 );
+            System.exit(0);
         }
+
         return question;
     }
-
-
-
 }
--- src/View/SoundManager.java
+++ src/View/SoundManager.java
@@ -3,53 +3,63 @@ package View;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import java.io.File;
+import java.io.BufferedInputStream;
+import java.io.InputStream;
 
 public class SoundManager {
     /**
-     * The sound click to be played.
+     * The sound clip to be played.
      */
     private Clip myClip;
     /**
      * The clip path.
      */
-    private final File[] mySoundURL;
+    private final String[] mySoundURL;
 
     /**
-     Constructs a SoundManager object.
-     Initializes the sound URL array and sets the file path for the sound.
+     * Constructs a SoundManager object.
+     * Initializes the sound URL array and sets the file path for the sound.
      */
     public SoundManager() {
-        mySoundURL = new File[1];
-        mySoundURL[0] = new File ("src/res/Sound/The Little Mermaid Under the Sea.wav");
+        mySoundURL = new String[1];
+        mySoundURL[0] = "/res/Sound/The Little Mermaid Under the Sea.wav";
     }
 
     /**
-     Sets the file to be played based on the specified index in the sound URL array.
-     @param theFile The index of the file in the sound URL array.
+     * Sets the file to be played based on the specified index in the sound URL array.
+     *
+     * @param theFile The index of the file in the sound URL array.
      */
     public void setFile(final int theFile) {
         try {
-            AudioInputStream ais = AudioSystem.getAudioInputStream(mySoundURL[theFile]);
+            InputStream is = getClass().getResourceAsStream(mySoundURL[theFile]);
+            AudioInputStream ais = AudioSystem.getAudioInputStream(new BufferedInputStream(is));
             myClip = AudioSystem.getClip();
             myClip.open(ais);
-        } catch(Exception e) {
+        } catch (Exception e) {
             System.out.println("Not found");
             e.printStackTrace();
         }
     }
+
     /**
-     Starts playing the audio clip.
+     * Starts playing the audio clip.
      */
     public void play() {
         myClip.start();
     }
+
     /**
-     Starts playing the audio clip in a loop.
+     * Starts playing the audio clip in a loop.
      */
     public void loop() {
         myClip.loop(Clip.LOOP_CONTINUOUSLY);
     }
 
+    /**
+     * Stops the audio clip.
+     */
+    public void stop() {
+        myClip.stop();
+    }
 }
-
